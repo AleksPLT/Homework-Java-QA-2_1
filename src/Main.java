764a18781b8a84fea1395df003a7f6299c431be7
Aleksandr Plotnikov <legendpro93@gmail.com>
2023-07-25 19:41:07 +0300

    Main.java change

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 68756;
+        int oneMilePrice = 20;
+        int bonusMiles = ticketPrice / oneMilePrice;
+        System.out.println("Количество бонусных миль: " + bonusMiles);
+    }
+}
